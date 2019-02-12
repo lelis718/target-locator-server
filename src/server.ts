62d0e11497527a790062bb05d91283748f89c86b
andre <andrelelis@gmail.com>
2019-02-11 23:35:50 -0200

    removal of ignored folders

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,8 @@ export class Server {
 
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
-        mongoose.connect("mongodb://localhost:27017/target-locator-db");
+        // tslint:disable-next-line:max-line-length
+        mongoose.connect("mongodb://lelis718:mcf1r3iioabo@target-locator-db-shard-00-00-a0swl.mongodb.net:27017,target-locator-db-shard-00-01-a0swl.mongodb.net:27017,target-locator-db-shard-00-02-a0swl.mongodb.net:27017/test?ssl=true&replicaSet=target-locator-db-shard-0&authSource=admin&retryWrites=true");
         mongoose.connection.on("error", (error) => {
             console.error(error);
         });
